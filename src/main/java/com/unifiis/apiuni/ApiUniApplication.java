23c67831ed702186829479b3444e5f826aaa2721
yeison herbert <yeisonherbert@gmail.com>
2021-12-12 01:16:42 -0500

    base

---
+++ src/main/java/com/unifiis/apiuni/ApiUniApplication.java
@@ -0,0 +1,13 @@
+package com.unifiis.apiuni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiUniApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiUniApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/unifiis/apiuni/ApiUniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unifiis.apiuni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiUniApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
